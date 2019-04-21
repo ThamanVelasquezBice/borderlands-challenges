1fd1e554661816475983e91ba4460b1a33ddfc7a
Thaman Velasquez-Bice <egahhd@gmail.com>
2019-04-21 16:32:38 -0400

    Add seeders

---
+++ database/seeds/GunBrandsSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GunBrandsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
