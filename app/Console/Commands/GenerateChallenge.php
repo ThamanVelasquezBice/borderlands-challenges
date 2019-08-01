99c91b44db5d9103433ccaa4489b251d8e2d3db4
ThamanVelasquezBice <egahhd@gmail.com>
2019-08-01 19:01:53 -0400

    support the challenges that have extra business logic(weapon type and brand exclusitivity)

--- app/Console/Commands/GenerateChallenge.php
+++ app/Console/Commands/GenerateChallenge.php
@@ -5,6 +5,8 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\Games;
 use App\Models\ChallengeTypes;
+use App\Models\ItemTypes;
+use App\Models\GunBrands;
 
 class GenerateChallenge extends Command
 {
@@ -76,11 +78,57 @@ class GenerateChallenge extends Command
 
         $challenge = ChallengeTypes::inRandomOrder()->first();
 
-        if ($character) {
-            echo 'Using '. $character->name .' your challenge will be: '. str_replace('_', ' ', $challenge->name) ."\r\n";
-        } else {
-            echo 'Your challenge will be '. str_replace('_', ' ', $challenge->name) ."\r\n";
+        $weapon_type = false;
+        $brand = false;
+
+        if ($challenge->name == 'weapon_type_only') {
+            $weapon_type = ItemTypes::where('is_a_weapon', 1)->where('game_id', $game->id)->inRandomOrder()->first();
         }
 
+        if ($challenge->name == 'brand_loyalty') {
+            $brand = GunBrands::where('game_id', $game->id)->inRandomOrder()->first();
+        }
+
+        if ($character && $weapon_type) {
+            $issued_challenge = 'Using '. $character->name .' your challenge will be: '. str_replace('_', ' ', $challenge->name) ." using the weapon type $weapon_type->name \r\n";
+        } else if ($character && $brand) {
+            $issued_challenge = 'Using '. $character->name .' your challenge will be: '. str_replace('_', ' ', $challenge->name) ." using the weapon brand $brand->name \r\n";
+        } else if ($character) {
+            $issued_challenge = 'Using '. $character->name .' your challenge will be: '. str_replace('_', ' ', $challenge->name) ."\r\n";
+        } else if ($weapon_type) {
+            $issued_challenge = 'Your challenge will be: '. str_replace('_', ' ', $challenge->name) ." using the weapon type $weapon_type->name \r\n";
+        } else if ($brand) {
+            $issued_challenge = 'Your challenge will be: '. str_replace('_', ' ', $challenge->name) ." using the weapon brand $brand->name \r\n";
+        } else {
+            $issued_challenge = 'Your challenge will be '. str_replace('_', ' ', $challenge->name) ."\r\n";
+        }
+
+        echo $issued_challenge;
+
+        $need_description = $this->choice('Do you need to know the challenge description?', ['no', 'yes']);
+
+        if ($need_description == 'yes') {
+            $need_description = true;
+        } else {
+            $need_description = false;
+        }
+
+        if ($need_description) {
+            echo $challenge->description->description."\r\n";
+        }
+
+        $bonus_rules = $this->choice('Would you like to apply bonus challenges?', ['no', 'yes']);
+
+        if ($bonus_rules == 'yes') {
+            $bonus_rules = true;
+        } else {
+            $bonus_rules = false;
+        }
+
+        if (!$bonus_rules) {
+            echo "Alright! Enjoy your challenge: \r\n $issued_challenge \r\n";
+        } else {
+
+        }
     }
 }
